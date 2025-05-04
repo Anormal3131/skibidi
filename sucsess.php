31bba908518f27251ec04371ed5c61d3ce7fa0df
Anormal3131 <139817517+Anormal3131@users.noreply.github.com>
2025-05-04 16:10:38 +0300

    Create sucsess.php

---
+++ sucsess.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <title>Başarılı! - Skibidi Pomni</title>
+</head>
+<body>
+    <h2>Key doğrulandı! Skibidi pomni, hoş geldin :)</h2>
+    <a href="index.php">Çıkış yap</a>
+</body>
+</html>
